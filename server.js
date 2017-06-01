ffd83e893c168a42fb6d0f6efdfc2b0a4a9697bf
Lucien Tavano <lucien@Luciens-MacBook-Pro.local>
2017-06-01 10:02:05 +0200

    server working

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var db = mongoose.connect('mongodb://localhost/swag-shop');
+
+var Product = require('./model/product');
+var WishList = require('./model/wishlist');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/product', function(request, response) {
+    
+})
 
 app.listen(3000, function(){
     console.log("Swag Shop API running on port 3000...");
 });
+
 
